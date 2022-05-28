f8fcaaeeaf213ffff5894423907ca5221933cdf0
leodan12 <leomacip4@gmail.com>
2022-05-28 00:40:30 -0500

    first commit

---
+++ src/main/java/com/ingweb/spring/web/app/ProyectoIngWebApplication.java
@@ -0,0 +1,13 @@
+package com.ingweb.spring.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIngWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIngWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingweb/spring/web/app/ProyectoIngWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingweb.spring.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIngWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
