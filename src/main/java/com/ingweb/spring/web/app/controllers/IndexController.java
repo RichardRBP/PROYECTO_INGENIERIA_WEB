7dc093efba3f3ca7745aa6a145ac7228df3469c3
Richard Braul <richardzz@hotmail.com>
2022-05-29 02:14:20 -0500

    Added Login

--- src/main/java/com/ingweb/spring/web/app/controllers/IndexController.java
+++ src/main/java/com/ingweb/spring/web/app/controllers/IndexController.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-<<<<<<< HEAD
-	
-	@GetMapping({"/index","/","/home"}) 
+
+	@GetMapping({"/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo","PROYECTO INGENIERIA WEB");
+		model.addAttribute("titulo", "PROYECTO INGENIERIA WEB");
 		return "index";
 	}
 	
-=======
-	//Llego luchito 
->>>>>>> aa013ed2be0cd4db6dc1e6c1cc620e367b1108dd
+	@GetMapping({ "/", "/login"})
+	public String login(Model model) {
+		model.addAttribute("login", "BIENVENIDO");
+		return "login";
+	}
+	
+	
+
 }
