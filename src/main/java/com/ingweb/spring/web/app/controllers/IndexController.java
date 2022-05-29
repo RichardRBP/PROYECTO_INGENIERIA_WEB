836bbbae634bf7f9a03dae39ab4efeaffad600e8
RobertoJose21 <rjvaldivia212000@gmail.com>
2022-05-29 06:22:22 -0500

    Added Home Template

--- src/main/java/com/ingweb/spring/web/app/controllers/IndexController.java
+++ src/main/java/com/ingweb/spring/web/app/controllers/IndexController.java
@@ -1,7 +1,18 @@
 package com.ingweb.spring.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
+	
+	@GetMapping({"/index","/","/home"}) 
+	public String index(Model model) {
+		model.addAttribute("titulo","PROYECTO INGENIERIA WEB");
+		return "index";
+	}
+	
 }
