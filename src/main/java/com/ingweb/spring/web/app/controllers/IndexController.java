72f53394b9a5dfda8647e31cb0f2128a42e8c97a
RobertoJose21 <rjvaldivia212000@gmail.com>
2022-05-30 01:27:17 -0500

    [MODIFY] Index Template

--- src/main/java/com/ingweb/spring/web/app/controllers/IndexController.java
+++ src/main/java/com/ingweb/spring/web/app/controllers/IndexController.java
@@ -8,15 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-<<<<<<< HEAD
 	
 	@GetMapping({"/index","/","/home"}) 
 	public String index(Model model) {
 		model.addAttribute("titulo","PROYECTO INGENIERIA WEB");
 		return "index";
 	}
-	
-=======
-	//Llego luchito 
->>>>>>> aa013ed2be0cd4db6dc1e6c1cc620e367b1108dd
 }
